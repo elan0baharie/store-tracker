70923b507b3a45e1d26aff7082a810bf872ec9c3
Elan Baharie <0elan1@gmail.com>
2017-05-12 09:22:18 -0800

    set up classes and make basic paths for site

--- app.rb
+++ app.rb
@@ -5,3 +5,12 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get('/') do
   erb(:index)
 end
+
+get('/stores/new') do
+  erb(:store_new)
+end
+
+get('/stores/view') do
+  @stores = Stores.all()
+  erb(:store_edit)
+end
--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -1,5 +1,5 @@
 require('spec_helper')
 
-describe(Class1) do
-  
+describe(Brand) do
+
 end
